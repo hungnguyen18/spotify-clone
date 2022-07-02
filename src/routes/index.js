026dacc0d9d5bc02a531ce0aecdc79f280c45406
hung <hungtrewq3214@gmail.com>
2022-07-02 18:57:43 +0700

    Router config

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import Home from '.././pages/Home';
+import Library from '.././pages/Library';
+
+const pageRoutes = [
+    { path: '/', component: Home },
+    { path: '/library', component: Library },
+];
+
+export default pageRoutes;
