0317ab0845541fba2872583cee17797b86674e8b
hung <hungtrewq3214@gmail.com>
2022-07-09 21:22:32 +0700

    Menu avatar user ui

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
-import { LogoutOutlined, UserOutlined } from '@ant-design/icons';
-import { Avatar, Tooltip } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { Avatar } from 'antd';
+import Tippy from '@tippyjs/react/headless';
 
 import styles from './Header.module.scss';
 import Search from '../Search';
 import apiClient from '../../spotify';
-import { ArrowDownIcon, ArrowUpIcon } from '../Icon';
+import { ArrowDownIcon, ArrowUpIcon, ShortcutIcon } from '../Icon';
 
 const cx = classNames.bind(styles);
 
-function Header({ shrink }) {
+function Header({ shrink, hideOnClick = true }) {
     const [infoUser, setInfoUser] = useState({});
     const [avatar, setAvatar] = useState('');
+    const [effect, setEffect] = useState({
+        backgroundColor: '',
+        iconUp: 'none',
+        iconDown: 'block',
+    });
 
     useEffect(() => {
         apiClient.get('me').then((response) => {
@@ -22,20 +28,88 @@ function Header({ shrink }) {
         });
     }, []);
 
+    const handleShowIcon = () => {
+        setEffect({
+            backgroundColor: '#282828',
+            iconUp: 'block',
+            iconDown: 'none',
+        });
+    };
+
+    const handleHideIcon = () => {
+        setEffect({
+            backgroundColor: '',
+            iconUp: 'none',
+            iconDown: 'block',
+        });
+    };
+
     return (
         <div className={cx('header__container', shrink)}>
             <div className={cx('header__search')}>{/* <Search /> */}</div>
 
-            <div className={cx('header__info')}>
-                <div className={cx('header__avatar')}>
-                    <Avatar size={28} src={avatar} icon={<UserOutlined />} />
+            <Tippy
+                interactive
+                trigger="click"
+                onShow={handleShowIcon}
+                onHide={handleHideIcon}
+                offset={[-20, 10]}
+                render={(attrs) => (
+                    <div className={cx('menu__body')} tabIndex="-1" {...attrs}>
+                        <div className={cx('menu__item')}>
+                            <span>Account</span>
+                            <ShortcutIcon />
+                        </div>
+                        <div className={cx('menu__item')}>
+                            <span>Profile</span>
+                        </div>
+                        <div className={cx('menu__item')}>
+                            <span>Upgrade to Premium</span>
+                            <ShortcutIcon />
+                        </div>
+                        <div className={cx('menu__item')}>
+                            <span>Support</span>
+                            <ShortcutIcon />
+                        </div>
+                        <div className={cx('menu__item')}>
+                            <span>Download</span>
+                            <ShortcutIcon />
+                        </div>
+                        <div className={cx('menu__item')}>
+                            <span>Log out</span>
+                        </div>
+                    </div>
+                )}
+            >
+                <div
+                    id="header__info"
+                    className={cx('header__info')}
+                    style={{ backgroundColor: effect.backgroundColor }}
+                >
+                    <div className={cx('header__avatar')}>
+                        <Avatar
+                            size={28}
+                            src={avatar}
+                            icon={<UserOutlined />}
+                        />
+                    </div>
+
+                    <span>{infoUser.display_name}</span>
+
+                    <div
+                        className={cx('icon-down')}
+                        style={{ display: effect.iconDown }}
+                    >
+                        <ArrowDownIcon />
+                    </div>
+                    <div
+                        className={cx('icon-up')}
+                        style={{ display: effect.iconUp }}
+                    >
+                        <ArrowUpIcon />
+                    </div>
                 </div>
-
-                <span>{infoUser.display_name}</span>
-
-                <ArrowDownIcon className={cx('icon-down')} />
-                <ArrowUpIcon className={cx('icon-up')} />
-            </div>
+            </Tippy>
         </div>
     );
 }
