f7a389102352cda9bd3a08824149c211bb9bdef3
hung <hungtrewq3214@gmail.com>
2022-07-19 20:58:56 +0700

    Ui header playlist

--- src/pages/Playlist/index.js
+++ src/pages/Playlist/index.js
@@ -12,12 +12,48 @@ function Playlist() {
     console.log(location);
 
     return (
-        <div className="container-full">
-            <div className={cx('playlist__header')}>Header</div>
+        <div className={cx('playlist__container')}>
+            <div className={cx('playlist__header')}>
+                <img
+                    src="https://i.scdn.co/image/ab67616d0000b273878ef5f909632a846601b51c"
+                    alt="Img"
+                    className={cx('playlist__img')}
+                />
 
-            <div className="container--not-padding-top">
-                <div className="border--bottom">
-                    <div className={cx('playlist__body')}>Body</div>
+                <div className={cx('playlist__info')}>
+                    <span className={cx('playlist__title')}>PLAYLIST</span>
+
+                    <h1 className={cx('playlist__name')}>Turn The Lights On</h1>
+
+                    <span className={cx('playlist__description')}>
+                        beats + birds
+                    </span>
+
+                    <div className={cx('playlist__details')}>
+                        <span className={cx('playlist__total')}>
+                            Spotify <span>123,905 likes</span>
+                            <span>123,905 likes</span>
+                        </span>
+
+                        <span className={cx('playlist__time')}>
+                            , about 6 hr
+                        </span>
+                    </div>
+                </div>
+            </div>
+
+            <div className={cx('playlist__body')}>
+                <div className="container--not-padding-top">
+                    <div className="border--bottom">
+                        <h1>Body</h1>
+                        <h1>Body</h1>
+                        <h1>Body</h1>
+                        <h1>Body</h1>
+                        <h1>Body</h1>
+                        <h1>Body</h1>
+                        <h1>Body</h1>
+                        <h1>Body</h1>
+                    </div>
                 </div>
             </div>
         </div>
